5a319f960b5db094b8b6fb23381d5f7fc9fbefc8
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-12-08 20:52:46 +0100

    Added color injector

---
+++ ReactionGame/Assets/Game/Scripts/Runtime/Configs/ColorConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorConfig : Configuration
+{
+    public enum Colors
+    {
+        Background, Accents
+    }
+
+    [SerializeField] private Color background = Color.white;
+    [SerializeField] private Color accents = Color.white;
+
+    public event Action ColorsChanged;
+
+    private void OnValidate()
+    {
+        ColorsChanged?.Invoke();
+    }
+
+    public Color GetColor(Colors color)
+    {
+        switch (color) {
+            case Colors.Background: return background;
+            case Colors.Accents: return accents;
+            default: return Color.black;
+        }
+    }
+}
---
+++ ReactionGame/Assets/Game/Scripts/Runtime/Misc/SpriteColorInjector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteColorInjector : MonoBehaviour
+{
+    [SerializeField] private ColorConfig.Colors color = ColorConfig.Colors.Background;
+    [SerializeField] private Color tint = Color.white;
+
+    private new SpriteRenderer renderer;
+
+    public Color Tint {
+        get => tint;
+        set {
+            tint = value;
+            SetColor();
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetColor();
+    }
+
+    private void Awake()
+    {
+        Configuration.Get<ColorConfig>().ColorsChanged += SetColor;
+    }
+
+    private void OnDestroy()
+    {
+        Configuration.Get<ColorConfig>().ColorsChanged -= SetColor;
+    }
+
+    void SetColor()
+    {
+        if (!renderer) {
+            renderer = GetComponent<SpriteRenderer>();
+        }
+        if (renderer) {
+            renderer.color = Configuration.Get<ColorConfig>().GetColor(color) * tint;
+        }
+    }
+}
