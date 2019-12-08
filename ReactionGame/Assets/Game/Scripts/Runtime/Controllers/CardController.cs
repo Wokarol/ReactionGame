bc528ca48e33eb3d3f868d231cac41ace1d4ab7d
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-12-08 20:53:12 +0100

    Added Cards

---
+++ ReactionGame/Assets/Game/Scripts/Runtime/Controllers/CardController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private Color normalTint = Color.white;
+    [SerializeField] private Color inactiveTint = Color.black * Color.white;
+    [Space]
+    [SerializeField] private float transitionTime = 0.2f;
+    [Header("Binds")]
+    [SerializeField] private SpriteColorInjector borderColor;
+    [SerializeField] private SpriteColorInjector patternColor;
+
+    [SerializeField, HideInInspector] private float transitionSpeed;
+
+    private float colorLerp = 1;
+    private float targetColorLerp = 1;
+
+    private void OnValidate()
+    {
+        transitionSpeed = 1 / transitionTime;
+    }
+
+    private void Update()
+    {
+        colorLerp = Mathf.MoveTowards(colorLerp, targetColorLerp, Time.deltaTime * transitionSpeed);
+        borderColor.Tint = patternColor.Tint
+            = Color.Lerp(inactiveTint, normalTint, colorLerp);
+    }
+
+    public void SetActive(bool v)
+    {
+        targetColorLerp = v ? 1 : 0;
+    }
+
+    public void SuddenSetActive(bool v)
+    {
+        targetColorLerp = colorLerp = v ? 1 : 0;
+    }
+
+    [ContextMenu("Set Active to TRUE")]
+    private void SetActiveToTrue() => SetActive(true);
+    [ContextMenu("Set Active to FALSE")]
+    private void SetActiveToFalse() => SetActive(false);
+}
