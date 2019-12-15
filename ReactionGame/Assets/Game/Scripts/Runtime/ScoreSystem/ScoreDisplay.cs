379ea40cd3d3234fab87ec06a51bc7f6d765f670
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-12-15 15:49:36 +0100

    Added score display

---
+++ ReactionGame/Assets/Game/Scripts/Runtime/ScoreSystem/ScoreDisplay.cs
@@ -0,0 +1,37 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private string scoreFormat = "0 000 000";
+    [SerializeField] private string deltaFormat = "000";
+    [Space]
+    [SerializeField] private TMPro.TMP_Text display = null;
+    [SerializeField] private TMPro.TMP_Text deltaDisplay = null;
+    [SerializeField] private ScoreController scoreController = null;
+
+    private void OnValidate()
+    {
+        if (!display) 
+            display = GetComponent<TMPro.TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        scoreController.PointsChanged += OnPointsChanged;
+    }
+    private void OnDisable()
+    {
+        scoreController.PointsChanged -= OnPointsChanged;
+    }
+
+
+    private void OnPointsChanged(int delta)
+    {
+        display.text = scoreController.Score.ToString(scoreFormat);
+        if (deltaDisplay)
+            deltaDisplay.text = delta.ToString(deltaFormat);
+    }
+}
